8dec6b8550537780f4344756b8fd5f3806235371
ccr <cchunren1997@gmail.com>
2021-08-14 19:00:20 +0800

    04 template update

--- template/03 template.cpp
+++ template/03 template.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int func01(int a, int b)
-{
-	return a + b;
-}
-
-template<typename T, typename V>
-int func02(T a, V b) {
-	return a + b;
-}
-
-void test01()
-{
-	int a = 10;
-	int b = 20;
-	char c = 'c';
-	long d = 12;
-	cout << func01(a, d) << endl;
-	cout << func02(a, c) << endl;
-
-}
-
-
-
-int main() {
-
-	test01();
-	return 0;
-}
+//#include <iostream>
+// #include <string>
+// using namespace std;
+// 
+// int func01(int a, int b)
+// {
+// 	return a + b;
+// }
+// 
+// template<typename T, typename V>
+// int func02(T a, V b) {
+// 	return a + b;
+// }
+// 
+// void test01()
+// {
+// 	int a = 10;
+// 	int b = 20;
+// 	char c = 'c';
+// 	long d = 12;
+// 	cout << func01(a, d) << endl;
+// 	cout << func02(a, c) << endl;
+// 
+// }
+// 
+// 
+// 
+// int main() {
+// 
+// 	test01();
+// 	return 0;
+// }
---
+++ template/04 template.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+void myprint(int a, int b) // if only sheng ming so it cant find the statement its want
+{
+	cout << "normal function called" << endl;
+}
+
+template<typename T>
+void myprint(T a,T b) {
+	cout << "template function called" << endl;
+}
+
+
+template<typename T>
+void myprint(T a, T b, T c) { //function overload
+	cout << "3para template function called" << endl;
+}
+
+
+void test01() {
+	int a = 10;
+	int b = 20;
+
+// 	myprint(a, b); // a and b is int type, so take void my print(int a, int b)
+// 	myprint<>(a, b); // a and b is int type,but with this symbol <> a blank template change make it as a template void myprint(T a,T b)
+// 	myprint<>(a, b, 100);// template can be overloaded
+
+	char c1 = 'a';
+	char c2 = 'b';
+	myprint(c1, c2); //template function called.Due to c1 and c2 are char types so machine dont do invisble change for it so this directly use template 
+}
+
+
+
+
+int main() {
+	test01();
+}
